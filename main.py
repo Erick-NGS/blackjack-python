7e0c23b133febb28b15a6d12d41a5df10fd6d8b7
Erick Silva <engs.3003@gmail.com>
2024-05-08 00:56:03 -0300

    updating main file due to utils

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from art import logo
-import random
+import blackjack_utils
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 user_cards = []
 computer_cards = []
 
-def deal_card():
-    """Deal a random card to an user"""
-    selected_card = random.choice(cards)
-    return selected_card
-
 # User cards
-user_cards.append(deal_card())
-user_cards.append(deal_card())
+user_cards.append(blackjack_utils.deal_card())
+user_cards.append(blackjack_utils.deal_card())
 
 # Computer cards
-computer_cards.append(deal_card())
-computer_cards.append(deal_card())
-
+computer_cards.append(blackjack_utils.deal_card())
+computer_cards.append(blackjack_utils.deal_card())
