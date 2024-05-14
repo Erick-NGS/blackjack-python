3b8b9b28a0a5e7aaa5b8a76efded3820d8931ca3
Erick Silva <engs.3003@gmail.com>
2024-05-14 15:52:57 -0300

    adding 'calculate_score' call on main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import blackjack_utils
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 # User cards
 user_cards.append(blackjack_utils.deal_card())
@@ -11,3 +12,8 @@ user_cards.append(blackjack_utils.deal_card())
 # Computer cards
 computer_cards.append(blackjack_utils.deal_card())
 computer_cards.append(blackjack_utils.deal_card())
+
+user_score = blackjack_utils.calculate_score(user_cards)
+computer_score = blackjack_utils.calculate_score(computer_cards)
+# print(f"User cards = {user_cards} / User score = {user_score}")
+# print(f"Computer 1st cards = {computer_cards[0]}")
